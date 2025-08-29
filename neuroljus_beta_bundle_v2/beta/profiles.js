c4e5d3fdb10427af1353ae60eb1373189cf5b584
NeuroLjus <elizabeth.ospinac@gmail.com>
2025-08-29 16:25:26 +0200

    Add files via upload

---
+++ neuroljus_beta_bundle_v2/beta/profiles.js
@@ -0,0 +1,64 @@
+window.NL_PROFILES = [
+  {
+    "id": "elizabeth",
+    "name": "Elizabeth",
+    "role": "founder",
+    "scopes": [
+      "admin",
+      "beta",
+      "editor"
+    ],
+    "hash": "056e845bb7f89b321059580a807c415dec35336f1dfdd62cfd4d4d84b64b5cce"
+  },
+  {
+    "id": "care_team",
+    "name": "Care Team",
+    "role": "caregiver",
+    "scopes": [
+      "beta",
+      "care"
+    ],
+    "hash": "c43cbd4d9c51037b659237020b1ecdfc258706c762650d28acb7778d5cec716e"
+  },
+  {
+    "id": "family",
+    "name": "Family",
+    "role": "family",
+    "scopes": [
+      "beta",
+      "view"
+    ],
+    "hash": "0090b678d528e216631c558eadc817b0b4d2780c1bb3a08301c23b8640209b17"
+  },
+  {
+    "id": "clinic_partner",
+    "name": "Clinic Partner",
+    "role": "clinician",
+    "scopes": [
+      "beta",
+      "clinical",
+      "view"
+    ],
+    "hash": "c85671978a939642def49ebd7cd4d54285cd40c440f7d89d2dc4f20d1ea43177"
+  },
+  {
+    "id": "research_partner",
+    "name": "Research Partner",
+    "role": "research",
+    "scopes": [
+      "beta",
+      "research",
+      "export"
+    ],
+    "hash": "155d2eef928d8618eb9b31d6fd7d48ecadfa3183314b833dad491f6708d87ce6"
+  },
+  {
+    "id": "public_demo",
+    "name": "Public Demo",
+    "role": "demo",
+    "scopes": [
+      "view"
+    ],
+    "hash": "176c325baac371796979a2b506047774c61c856888bb1549f1e6e5126c444053"
+  }
+];
